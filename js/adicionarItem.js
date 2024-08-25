c2fb738b5001f87899c4a9dcf4556d29b7b20667
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-08-25 20:54:46 -0300

    fim

--- js/adicionarItem.js
+++ js/adicionarItem.js
@@ -1,9 +1,8 @@
 import { criarItemDaLista } from "./criarItemDaLista.js";
 import { verificarListaVazia } from "./verificarListaVazia.js";
 
-export function adicionarItem(evento){
-    evento.preventDefault();
-
+export function adicionarItem(pEvento){
+    pEvento.preventDefault();
     const lCampoDeEntradaDoItemDeCompra = document.getElementById("input-item");
     const lListaDeCompras = document.getElementById("lista-de-compras");
     const lNomeDoItem = lCampoDeEntradaDoItemDeCompra.value;
